cc07fe99d9d5dcb9e5cc5de3b942a1c5f5be84c2
alliano <allianoanonymous@gmail.com>
2022-12-12 16:29:09 +0900

    create method createPassword

--- account-service/src/main/java/com/account/accountservice/controllers/interfaces/AccountController.java
+++ account-service/src/main/java/com/account/accountservice/controllers/interfaces/AccountController.java
@@ -3,10 +3,16 @@ package com.account.accountservice.controllers.interfaces;
 import org.springframework.http.ResponseEntity;
 
 import com.account.accountservice.dtos.AccountRegisterDto;
+import com.account.accountservice.dtos.PasswordDto;
+import com.account.accountservice.dtos.VerifivationOtpDto;
 
 public interface AccountController {
 
     public ResponseEntity<?> register(AccountRegisterDto accountRegisterDto);
 
     public String load();
+
+    public ResponseEntity<?> verification(VerifivationOtpDto verifivationOtp);
+
+    public ResponseEntity<?> createPassword(PasswordDto password);
 }
