0fe1773a5358cac6e4ad8f363ec5841bc76dd3fc
alliano <allianoanonymous@gmail.com>
2022-12-12 16:30:35 +0900

    add an annotation @Indexed from redis

--- account-service/src/main/java/com/account/accountservice/domain/entities/tmpentities/TmpAccount.java
+++ account-service/src/main/java/com/account/accountservice/domain/entities/tmpentities/TmpAccount.java
@@ -3,17 +3,19 @@ package com.account.accountservice.domain.entities.tmpentities;
 import javax.persistence.Id;
 
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter @Getter
-@RedisHash(value = "account", timeToLive = 1000)
+@RedisHash(value = "account", timeToLive = 4000)
 public class TmpAccount {
 
     @Id
     private String id;
 
+    @Indexed
     private String email;
 
     private boolean valid = false;
